4e3511ea793b07196ddaddaf629dea55698d2a99
tenessy0570 <tenessy0570@gmail.com>
2023-02-09 15:11:53 +0700

    created model. Migrated

--- django_stripe/api/models.py
+++ django_stripe/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    description = models.TextField(
+        null=False,
+        blank=False
+    )
+    price = models.IntegerField(null=False, blank=False)
