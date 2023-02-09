d3e4dc74234b39fd349b1f43c6b4e44b412e209c
tenessy0570 <tenessy0570@gmail.com>
2023-02-09 16:20:44 +0700

    updated buy method to get stripe session url

--- django_stripe/api/urls.py
+++ django_stripe/api/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('item/<int:item_id>', views.ItemView.as_view(), name="get_item")
+    path('item/<int:item_id>', views.ItemView.as_view(), name="get_item"),
+    path('buy/<int:item_id>', views.BuyView.as_view(), name="buy_item"),
 ]
--- django_stripe/api/views.py
+++ django_stripe/api/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+import stripe
 
-# Create your views here.
-from requests import Request
+from django.shortcuts import render
+from django.urls import reverse
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Item
 
+stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
 
 class ItemView(APIView):
-    def get(self, request: Request, item_id):
+    def get(self, request, item_id):
         item = get_object_or_404(Item, id=item_id)
-        return render(request, 'api/item.html', context={"item": item})
+        url_to_buy_item = reverse("buy_item", args=[item_id])
 
+        return render(request, 'api/item.html', context={"item": item, "buy_url": url_to_buy_item})
+
+
+class BuyView(APIView):
+    def get(self, request, item_id):
+        item = get_object_or_404(Item, id=item_id)
+
+        session: stripe.checkout.Session = stripe.checkout.session.Session.create(success_url="https://vk.com", mode="payment", line_items=[
+            {"price": stripe.Price.create(
+                unit_amount=item.price * 100,
+                currency="usd",
+                recurring=None,
+                product=stripe.Product.create(name=item.name)
+            ),
+                "quantity": 1
+            }
+        ])
+        return Response(data={"session_id": session.id})
