3602975670e9da65d81bfa928142e45950c4374c
tenessy0570 <tenessy0570@gmail.com>
2023-02-09 15:32:41 +0700

    updated view. updated templates

--- django_stripe/api/views.py
+++ django_stripe/api/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from requests import Request
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .models import Item
 
 
 class ItemView(APIView):
     def get(self, request: Request, item_id):
-        return render(request, 'api/item.html', context={"item_id": item_id})
+        item = get_object_or_404(Item, id=item_id)
+        return render(request, 'api/item.html', context={"item": item})
 
